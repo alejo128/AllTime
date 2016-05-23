68b45bfef665afc224b37a6b99b8ac4935e48dcb
jbecerr2 <j_mbc95@hotmail.com>
2016-05-23 14:03:34 -0500

    perfeccionado el json, agregado también el output en xml

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,8 +2,20 @@ class PlacesController < ApplicationController
   #GET /places
   def index
     @a = Place.all
+
+    respond_to do | f |
+      f.html
+      f.json {
+        render json: @a
+      }
+      f.xml {
+        render xml: @a
+      }
+    end
+
     #render :json => @a
   end
+
   def show
      @places = Places.find params[:all]
   end
--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -2,6 +2,15 @@ class SchedulesController < ApplicationController
   #GET /schedules
   def index
     @b = Schedule.all
+    respond_to do | f |
+      f.html
+      f.json {
+        render json: @b
+      }
+      f.xml {
+        render xml: @b
+      }
+    end
     #render :json =>  @b
   end
   json = @b.to_json
